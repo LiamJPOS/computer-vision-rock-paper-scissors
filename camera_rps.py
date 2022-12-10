950fc234991e1a7fd1b479f0a5ae30cfe5bae290
Liam <liam.oshaughnessy@live.co.uk>
2022-12-10 11:55:52 +0000

    Timer implemented and game ending logic implemented

--- camera_rps.py
+++ camera_rps.py
@@ -23,7 +23,9 @@ def get_prediction():
 def get_user_choice():
     prediction_array = get_prediction()
     move_code = np.argmax(prediction_array)
-    return mapper(move_code)    
+    user_choice = mapper(move_code)    
+    print(f'You have chosen {user_choice}')
+    return user_choice
 
 def get_computer_choice ():
     computer_choice = random.choice(["rock", "paper", "scissors"])
@@ -33,42 +35,44 @@ def get_computer_choice ():
 def get_winner(computer_choice, user_choice): 
     if user_choice == computer_choice:
         print("It is a tie!")
+        return 'tie'
         
     elif user_choice == "rock":
         if computer_choice == "paper":
             print("You lost")  
+            return 'computer'
         else: 
             print("You Won!")
-                  
+            return 'user'
+
     elif user_choice == "paper":
         if computer_choice == "scissors":
-            print("You lost")     
+            print("You lost")   
+            return 'computer'  
         else:
             print("You won!")
+            return 'user'
             
     elif user_choice == "scissors":
         if computer_choice == "rock":
-            print("You lost")    
+            print("You lost")   
+            return 'computer' 
         else: 
             print("You won!")
-
-#TODO Create new countdown method that doesn't interfere with camera input
-def countdown(time_sec):
-    print("Get ready! Choose Rock, Paper, or Scissors!")
-    while time_sec > 0:
-        end_time = time.time() + 1
-        time_sec -= 1
-        print(time_sec)
-        while time.time() < end_time:
-            pass
+            return 'user'
 
 model_path = 'C:\\Users\\liamo\\Code\\computer-vision-rock-paper-scissors\\keras_model.h5'
 model = load_model(model_path)
 rounds_played = 0
+user_wins = 0 
+computer_wins = 0
+countdown = 5
 
 #Open webcam in CV
 capture = cv2.VideoCapture(0)
-while True: 
+while True:
+    
+    #Read and display frame 
     ret, frame = capture.read()
     
     #standardize frame size
@@ -77,28 +81,80 @@ while True:
     cv2.rectangle(frame, (10, 240), (214, 470), (255,255,255), thickness=2)
     #computer rectangle 
     cv2.rectangle(frame, (426, 240), (630, 470), (255,255,255), thickness=2)  
-
-    #extract and resize region in user rectangle
-    user_frame = frame[244:466, 14:204] #takes [y:y+h,x:x+w]
-    resized_user_frame = cv2.resize(user_frame, (224, 224), interpolation = cv2.INTER_AREA)
-
-    #predict user move
-    user_choice = get_user_choice()
-    print(f'You have chosen {user_choice}')
     
-    #get the winner
-    if user_choice != 'none':
-
-        computer_choice = get_computer_choice()
-        get_winner(computer_choice, user_choice)
-        rounds_played += 1
+    cv2.putText(frame, 'Let\'s play Rock, Paper, Scissors', (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), thickness=2)
+    cv2.putText(frame, 'Press Q to start the round', (10, 70), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (255, 255, 255), thickness=2)
 
     cv2.imshow('Rock Paper Scissors', frame)
     
-    if cv2.waitKey(1) & 0xFF == ord('q'):
+    k = cv2.waitKey(125)
+    if k == ord('q'):
+        previous_time = time.time()
+        
+        while countdown >= 0:
+            ret, frame = capture.read()
+            
+            #Display countdown on each frame
+            cv2.putText(frame, 'Choose rock, paper, or scissors', (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), thickness=2)
+            cv2.putText(frame, str(countdown), (300, 200), cv2.FONT_HERSHEY_SIMPLEX, 5, (255, 255, 255), thickness=2)
+            #user rectangle 
+            cv2.rectangle(frame, (10, 240), (214, 470), (255,255,255), thickness=2)
+            
+            cv2.imshow('Rock Paper Scissors', frame)
+            cv2.waitKey(125)
+            
+            current_time = time.time()
+            
+            if current_time - previous_time >= 1:
+                previous_time = current_time
+                countdown -= 1
+        else:
+            ret, frame = capture.read()
+            
+            #extract and resize region in user rectangle
+            user_frame = frame[244:466, 14:204] #takes [y:y+h,x:x+w]
+            resized_user_frame = cv2.resize(user_frame, (224, 224), interpolation = cv2.INTER_AREA)
+            
+            #predict user move
+            user_choice = get_user_choice()
+            
+            #get the winner
+            if user_choice != 'none':
+
+                computer_choice = get_computer_choice()
+                winner = get_winner(computer_choice, user_choice)
+                if winner == 'user':
+                    user_wins += 1
+                if winner == 'computer':
+                    computer_wins += 1
+                    
+                rounds_played += 1
+                
+            else: 
+                computer_choice = 'none'
+                print('Choose rock, paper or scissors when the timer reaches 0')
+        
+        countdown = 5
+        
+    #Press esc to close game
+    if cv2.waitKey(1) & k == 27:
+        break
+    if user_wins == 3:
+        print('You have won the most rounds! You win!')
+        break
+        
+    if computer_wins == 3:
+        print('The computer has won the most rounds. You lost.')
         break
     if rounds_played == 5:
+        if user_wins > computer_wins:
+            print('You have won the most rounds! You win!')
+        elif computer_wins > user_wins:
+            print('The computer has won the most rounds. You lost.')
+        else:
+            print('You both tied. 5 ties in a row is a 0.41\% chance!')
         break
+    
     
 capture.release()
 cv2.destroyAllWindows
