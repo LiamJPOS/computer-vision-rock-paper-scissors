2e8851259b75ac54d36adb5bfe1bd25d5380eda9
Liam <paledeepened@gmail.com>
2023-12-26 14:16:51 +0000

    Implemented user choice and computer choice functions.

--- manual_rps.py
+++ manual_rps.py
@@ -1,5 +1,4 @@
 import random
-rps = ['rock', 'paper', 'scissors']
 
 def get_computer_choice():
     return random.choice(rps)
@@ -18,8 +17,8 @@ def get_winner(computer_choice, user_choice):
     if user_choice == computer_choice:
         print("It is a tie!")
     elif (user_choice == 'rock' and computer_choice == 'scissors') or \
-         (user_choice == 'paper' and computer_choice == 'rock') or \
-         (user_choice == 'scissors' and computer_choice == 'paper'):
+        (user_choice == 'paper' and computer_choice == 'rock') or \
+        (user_choice == 'scissors' and computer_choice == 'paper'):
         print("You won!")
     else:
         print("You lost")
@@ -29,4 +28,5 @@ def play():
     user_choice = get_user_choice()
     get_winner(computer_choice, user_choice)    
     
+rps = ['rock', 'paper', 'scissors']
 play()
