f568e7462caaa57f180fee0afc19ee4fa2fb6543
Liam <paledeepened@gmail.com>
2024-01-15 18:39:38 +0000

    Implement method to predict user move

--- play_rps.py
+++ play_rps.py
@@ -14,13 +14,6 @@ import cv2
 from keras.models import load_model
 import numpy as np
 
-class Rps():
-    def __init__(self):
-        self.user_wins = 0
-        self.computer_wins = 0
-        self.rounds_played = 0
-        self.countdown = 0
-
 class Screen_square():
     def __init__(self, coordinates):
         self.x1, self.y1, self.x2, self.y2 = coordinates
@@ -30,17 +23,43 @@ class Screen_square():
     
 class User_square(Screen_square):
     def __init__(self, model=False):
-        self.model = model
-    #Convert image in square to an np array
-    def convert_image(self):
-        pass
-    #Return model prediction of np array converted from image in square
+        self.model = load_model(model)
+        self.prediction_array = np.ndarray(shape=(1, 224, 224, 3), dtype=np.float32)
+    #Returns normalized image array of area in square to an np array usable in keras model
+    def convert_image(self, frame):
+        extracted_region = frame[self.y1:self.x1, self.y2:self.x2]
+        extracted_region = cv2.cvtColor(extracted_region, cv2.COLOR_BGR2RGB)
+        extracted_region = cv2.resize(extracted_region, (224, 224), interpolation = cv2.INTER_AREA)
+        image_array = np.array(extracted_region)
+        normalized_image_array = (image_array.astype(np.float32) / 127.0) - 1 # Normalize the image
+        return normalized_image_array
+    #Returns model prediction of np array converted from area of image in square
     #rock, paper, scissors, nothing
-    def get_predicted_move(self):
-        pass
+    def get_user_move(self):
+        self.prediction_array[0] = self.convert_image(frame)
+        model_prediction = self.model.predict(self.prediction_array)
+        prediction_index = np.argmax(model_prediction)
+        predicted_move = ['rock', 'paper', 'scissors', 'nothing'][prediction_index]
+        return predicted_move
 
 class Computer_square(Screen_square):
-    pass
+    def get_computer_choice(self):
+        return random.choice(['rock', 'paper', 'scissors'])
+    
+    def show_choice_icon(self):
+        pass
+
+class Rps():
+    def __init__(self):
+        self.user_wins = 0
+        self.computer_wins = 0
+        self.rounds_played = 0
+        self.countdown = 0
+
+    #TODO put game logic
+    #TODO implement countdown logic here
+    def start_round(self):
+        pass
 
 cap = cv2.VideoCapture(0)
 user_square = User_square(coordinates=[408, 216, 632, 440], model="keras_model.h5")
